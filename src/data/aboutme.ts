675b0e099d3c08c55b092ef688dc8bf8b306dd8c
Kamala Pun <punkamala197@gmail.com>
2026-06-04 15:46:17 -0500

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -24,7 +24,7 @@ export const aboutMe: AboutMe = {
   // Note that links work in the description
   description:
     "I am an aspiring researcher in Cybersecurity with research interests in IoT security, cloud infrastructure, secure DevOps, and threat modeling. I hold a Master’s degree in Computer Science from <a href='https://www.stcloudstate.edu/'>St. Cloud State University</a>, where my research included a starred paper on IoT-based applications and applied projects using Azure IoT and data-driven analysis.My background combines graduate teaching experience, secure software development, and enterprise system administration, with hands-on experience in cloud platforms, CI/CD pipelines, and authentication frameworks. I am particularly interested in securing distributed and cloud-native systems by bridging practical system engineering with academic research.",
-  email: "kamala.pun0423@gmail.com",
+  //email: "kamala.pun0423@gmail.com",
   imageUrl:
   "https://media.licdn.com/dms/image/v2/D5603AQElsbJkQ21Z4g/profile-displayphoto-crop_800_800/B56Z42WlcHKIAI-/0/1779028352961?e=1782345600&v=beta&t=7OZ7wpGS9L0CXkRg_01LUC5RaxBU3DFl3xJm_lTJk1Y",
   //googleScholarUrl: "https://scholar.google.com/",
